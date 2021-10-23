877cae78d055c967d7e4c45d24d98cae1b339157
Kirill Nikolaev <kn@typex.one>
2021-10-24 00:35:36 +0300

    All works

--- src/main/java/com/example/roadscleanup/RoadsCleanupApplication.java
+++ src/main/java/com/example/roadscleanup/RoadsCleanupApplication.java
@@ -8,6 +8,7 @@ import org.jgrapht.Graph;
 import org.jgrapht.alg.interfaces.ShortestPathAlgorithm;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultDirectedWeightedGraph;
 import org.jgrapht.graph.DefaultEdge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -39,7 +40,7 @@ public class RoadsCleanupApplication {
             List<JsonFeature> result = gson.fromJson(paths, listType);
             var dots = new ArrayList<Point>();
             var streetNames = new HashMap<Long, String>();
-            Graph<Long, DefaultEdge> g = new DefaultDirectedGraph<>(DefaultEdge.class);
+            Graph<Long, DefaultEdge> g = new DefaultDirectedWeightedGraph<>(DefaultEdge.class);
             HashMap<Long, List<Point>> dotsByStreetId = new HashMap<>();
             for (JsonFeature jsonFeature : result) {
                 var streetId = jsonFeature.getProperties().getId();
@@ -58,7 +59,7 @@ public class RoadsCleanupApplication {
                     if (dot1.getStreetId() == dot2.getStreetId()) {
                         continue;
                     }
-                    if (Math.sqrt(Math.pow(dot1.getLat() - dot2.getLat(), 2.0) + Math.pow(dot1.getLon() - dot2.getLon(), 2.0)) <= 0.0001) {
+                    if (Math.sqrt(Math.pow(dot1.getLat() - dot2.getLat(), 2.0) + Math.pow(dot1.getLon() - dot2.getLon(), 2.0)) <= 0.0005) {
                         System.out.println(streetNames.get(dot1.getStreetId()) + " -> " + streetNames.get(dot2.getStreetId()));
                         g.addEdge(dot1.getStreetId(), dot2.getStreetId());
                         g.addEdge(dot2.getStreetId(), dot1.getStreetId());
@@ -66,28 +67,41 @@ public class RoadsCleanupApplication {
                 }
             }
 
-            DijkstraShortestPath<Long, DefaultEdge> dijkstraAlg = new DijkstraShortestPath<>(g);
-            ShortestPathAlgorithm.SingleSourcePaths<Long, DefaultEdge> iPaths = dijkstraAlg.getPaths(16491L);
-            double max = 0.0;
-            long maxV = 16491L;
-            for (Long aLong : streetNames.keySet()) {
-                double h = iPaths.getWeight(aLong);
-                if (h > max && h < 1000) {
-                    max = h;
-                    maxV = aLong;
-                }
-            }
 
-            System.out.println(max);
 
-            var out = new ArrayList<List<Point>>();
-            for (Long aLong : iPaths.getPath(maxV).getVertexList()) {
-                var ff = dotsByStreetId.get(aLong);
-                var ffr = new ArrayList<Point>();
-                for (Point point : ff) {
-                    ffr.add(new Point(point.getLat(), point.getLon(), point.getStreetId()));
+            for (int i = 0; i < 3; i++) {
+                DijkstraShortestPath<Long, DefaultEdge> dijkstraAlg = new DijkstraShortestPath<>(g);
+                ShortestPathAlgorithm.SingleSourcePaths<Long, DefaultEdge> iPaths = dijkstraAlg.getPaths(16491L);
+                double max = 0.0;
+                long maxV = 16491L;
+                for (Long aLong : streetNames.keySet()) {
+                    double h = iPaths.getWeight(aLong);
+                    if (h > max && h < 1000) {
+                        max = h;
+                        maxV = aLong;
+                    }
                 }
-                out.add(ffr);
+
+                System.out.println(max);
+                var out = new ArrayList<List<Point>>();
+                for (Long aLong : iPaths.getPath(maxV).getVertexList()) {
+                    for (DefaultEdge defaultEdge : g.edgesOf(aLong)) {
+                        if (Objects.equals(g.getEdgeTarget(defaultEdge), aLong)) {
+                            g.setEdgeWeight(defaultEdge, g.getEdgeWeight(defaultEdge) / 10);
+                        }
+                    }
+                    var ff = dotsByStreetId.get(aLong);
+                    var ffr = new ArrayList<Point>();
+                    for (Point point : ff) {
+                        ffr.add(new Point(point.getLat(), point.getLon(), point.getStreetId()));
+                    }
+                    out.add(ffr);
+                }
+
+                System.out.println(max);
+                var name = "./test" + i + ".json";
+                new File(name).createNewFile();
+                Files.writeString(Path.of(name), gson.toJson(out));
             }
 /*
             Point prevPoint = null;
@@ -146,9 +160,7 @@ public class RoadsCleanupApplication {
 */
 
 
-            System.out.println(max);
-            new File("./test.json").createNewFile();
-            Files.writeString(Path.of("./test.json"), gson.toJson(out));
+
 
         } catch (IOException e) {
             e.printStackTrace();
