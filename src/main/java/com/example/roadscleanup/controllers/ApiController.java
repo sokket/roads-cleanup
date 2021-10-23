45251a46330bd119fab6a8b941e388755ce60801
xinik <sergey20160@gmail.com>
2021-10-23 16:50:03 +0300

    Add street api

--- src/main/java/com/example/roadscleanup/controllers/ApiController.java
+++ src/main/java/com/example/roadscleanup/controllers/ApiController.java
@@ -3,10 +3,15 @@ package com.example.roadscleanup.controllers;
 import com.example.roadscleanup.dto.VehiclesApiResp;
 import com.example.roadscleanup.dto.VehiclesLocationApiResp;
 import com.example.roadscleanup.services.ApiService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Optional;
 import java.util.Set;
 
 @RestController
@@ -27,6 +32,18 @@ public class ApiController {
     @GetMapping("locations")
     public Set<VehiclesLocationApiResp> getVehiclesLocation() {
         return routsService.getVehiclesLocation();
+    }
+
+    @GetMapping("streets")
+    public ResponseEntity<String> getStreetData() {
+        try {
+            return ResponseEntity.of(
+                    Optional.of(Files.readString(
+                            Paths.get("/home/xinik/IdeaProjects/roads-cleanup/src/main/resources/routes.json"))));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.internalServerError().build();
     }
 
 }
